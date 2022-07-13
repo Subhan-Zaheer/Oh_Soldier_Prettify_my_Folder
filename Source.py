d57140132c4ae8d9e12aff17efd0587447d0becf
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-07-13 23:09:41 +0500

    Some changes done like we made function a co routine and make our function more generalize

--- Source.py
+++ Source.py
@@ -7,23 +7,28 @@ def soldier(path, file_name, fileFormat):
     text = file.read()
     print(text)
     file.close()
-    for item in os.listdir(path):
-        extension = os.path.splitext(item)
-        # print(extension)
+    counter = 1
+    while True:
+        text1 = (yield)
+        print(f"Text 1 is : {text1}" )
+        for item in os.listdir(path):
+            extension = os.path.splitext(item)
+            # print(extension)
 
-        if extension[1] == ".jpg" or extension[1] == ".jpeg":
+            if extension[1] == fileFormat or extension[1] == fileFormat:
 
-            os.rename(item, f"1{extension[1]}")
-        if extension[0] in text:
-            continue
-        else:
-            if item[0].isalpha():
-                print(item[0])
-                os.rename(item, item.capitalize())
+                os.rename(item, f"{counter}{extension[1]}")
+                counter = counter + 1
+            if extension[0] in text:
+                continue
+            else:
+                if item[0].isalpha():
+                    print(item[0])
+                    os.rename(item, item.capitalize())
 
-        print((item))
-        print((extension))
-    pass
+            print((item))
+            print((extension))
+        pass
 
 
 
@@ -32,7 +37,7 @@ if __name__ == '__main__':
     # path = input("Enter the path here : ")
     # fileName = input("Enter file name here of which you dont want to captilize first letter : ")
     # fileFormat = input("Enter file format you want to rename the files : ")
-    soldier("C:\\Users\\Khan Mob s  Comp\\PycharmProjects\\AnotherProj", "Subhan2.txt", ".jpg")
-    # s = "subhan.txt"
-    # s = s.capitalize()
-    # print(s)
+    soldierFunc = soldier("C:\\Users\\Khan Mob s  Comp\\PycharmProjects\\AnotherProj", "Subhan2.txt", ".jpeg")
+    next(soldierFunc)
+    soldierFunc.send(0)
+    
